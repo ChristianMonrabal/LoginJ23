16eb73349937464791e7d17f719099421f17d89f
Dmasc67 <sergi.masip2004@gmail.com>
2024-10-10 19:19:33 +0200

    New SQL, new method using try-conn

--- includes/conexion.php
+++ includes/conexion.php
@@ -4,12 +4,6 @@ $db = 'db_escuelaJ23';
 $user = 'root';
 $pass = '';
 
-//$conn = new mysqli($host, $user, $pass, $db);
-
-//if ($conn->connect_error) {
-//    die("Conexión fallida: " . $conn->connect_error);
-//}
-
 try {
     $conn =mysqli_connect($host,$user,$pass,$db);
 } catch (Exception $e) {
